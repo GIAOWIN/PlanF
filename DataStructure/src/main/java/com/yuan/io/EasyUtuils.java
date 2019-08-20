a2b2c2ad6236dec11c7e970dd8cce092ff3d40ff
yuan <yuan___9826@163.com>
2019-08-20 21:14:20 +0800

    增加两个方法
    一个拷贝
    一个批量拷贝

---
+++ DataStructure/src/main/java/com/yuan/io/EasyUtuils.java
@@ -0,0 +1,50 @@
+package com.yuan.io;
+
+import java.io.*;
+
+public class EasyUtuils {
+
+    /**
+     * 复制一个文件
+     * @param target      目标
+     * @param destination 目的地
+     */
+    public static void copy(File target, String destination) {
+        try (
+                FileInputStream in = new FileInputStream(target);
+                FileOutputStream out = new FileOutputStream(destination+"\\"+target.getName())
+        ) {
+
+            int len;
+            final byte[] bs = new byte[1024 * 10];
+            while ((len = in.read(bs)) != -1) {
+                out.write(bs,0,len);
+                System.out.println(destination+target.getName());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 复制一个文件夹的文件
+     * @param target      目标
+     * @param destination 目的地
+     * @param type 文件类型
+     */
+    public static void copyAll(File target, String destination, String type) {
+        if (target.isDirectory()) {
+            //如果是文件夹就递归
+            for (File file : target.listFiles()) {
+                copyAll(file, destination, type);
+            }
+        } else {
+            //是文件就过滤.pdf结尾的
+            if (target.getName().endsWith(type)) {
+                //是就调用复制方法
+                copy(target, destination);
+            }
+        }
+    }
+}
